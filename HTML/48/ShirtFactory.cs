12104c3838f124f913d8c476d78bb76329794f73
Mordechai Klein <mordkln33@gmail.com>
2026-06-15 10:22:54 -0400

    class 48

---
+++ HTML/48/ShirtFactory.cs
@@ -0,0 +1,53 @@
+﻿namespace ShirtFactory
+{
+    internal class Shirt
+    {
+        public static string[] colorOptions = { "red", "blue", "green" };
+        public static string[] patternOptions = { "striped", "plain", "polka dot" };
+        string color;
+        string pattern;
+
+        public Shirt(string color, string pattern)
+        {
+            this.color = color;
+            this.pattern = pattern;
+        }
+
+        public override string? ToString()
+        {
+            return $"Shirt: {color}, Pattern: {pattern}";
+        }
+    }
+
+    internal class ShirtFactory
+    {
+
+        public static List<Shirt> CreateAllShirts()
+        {
+            List<Shirt> allShirts = new List<Shirt>();
+
+            for (int i = 0; i < Shirt.colorOptions.Length; i++)
+            {
+                for (int j = 0; j < Shirt.patternOptions.Length; j++)
+                {
+                    allShirts.Add(new Shirt(Shirt.colorOptions[i], Shirt.patternOptions[j]));
+                }
+            }
+            return allShirts;
+        }
+
+        public static void PrintShirts(List<Shirt> shirts)
+        {
+            foreach (Shirt shirt in shirts)
+            {
+                Console.WriteLine(shirt);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Shirt> newShirtList = CreateAllShirts();
+            PrintShirts(newShirtList);
+        }
+    }
+}
